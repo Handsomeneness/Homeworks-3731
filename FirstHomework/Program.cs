044e45112ec783b3d7a55dabbc8759eb1f0e608c
Handsomeness <Handsomenes@yandex.ru>
2023-01-16 18:11:31 +0400

    Решены 2 задачи

--- FirstHomework/Program.cs
+++ FirstHomework/Program.cs
@@ -10,6 +10,50 @@ namespace FirstHomework
     {
         static void Main(string[] args)
         {
+            WhoseGreater();
+            Console.WriteLine();
+            Maximum();
+            Console.WriteLine();
         }
+
+        public static void WhoseGreater()
+        {
+            Console.WriteLine("Введите первое число");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            double b = double.Parse(Console.ReadLine());
+
+            if (a > b)
+                Console.WriteLine($"Первое число: {a} больше второго: {b}");
+            else
+                Console.WriteLine($"Второе число: {b} больше первого: {a}");
+        }
+
+
+        public static void Maximum()
+        {
+            Console.WriteLine("Введите первое число");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите третье число");
+            double c = double.Parse(Console.ReadLine());
+
+            if (a > b && a > c)
+                Console.WriteLine($"Первое число максимальное: {a}");
+
+            else if (b > a && b > c)
+                Console.WriteLine($"Второе число максимальное: {b}");
+
+            else 
+                Console.WriteLine($"Третье число максимальное: {c}");
+        }
+
+       
+
+
     }
 }
