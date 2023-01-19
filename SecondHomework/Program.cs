e8f39aea9a52a96737798715fc9bae4ab271a87d
Handsomeness <Handsomenes@yandex.ru>
2023-01-19 22:09:19 +0400

    Решены все задачи

--- SecondHomework/Program.cs
+++ SecondHomework/Program.cs
@@ -10,9 +10,11 @@ namespace SecondHomework
     {
         static void Main(string[] args)
         {
-            //ThreeDigitNumber();
-            //Console.WriteLine();
+            ThreeDigitNumber();
+            Console.WriteLine();
             ThirdNumber();
+            Console.WriteLine();
+            DayOfWeek();
         }
 
         public static void ThreeDigitNumber()
@@ -35,6 +37,19 @@ namespace SecondHomework
             string result = count.Length < 3 || count.Substring(0, 1) == "-" ?
                 "В вашем числе нет третьей цифры или оно отрицательное" : count.Substring(2, 1);
             Console.WriteLine(result);
+        }
+
+        public static void DayOfWeek()
+        {
+            Console.WriteLine("Введите день недели");
+            int day = int.Parse(Console.ReadLine());
+
+            if (day >= 1 && day <= 5)
+                Console.WriteLine("Будний день, пора на галеру");
+            else if (day <= 0 || day > 7)
+                Console.WriteLine("Не в этой вселенной");
+            else
+                Console.WriteLine("Выходной! Ведьмак не ждёт!");
         }
     }
 }
