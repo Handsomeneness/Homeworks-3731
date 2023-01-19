d530d6c222548f4242dfc2775dd48f8bf4d4f6b8
Handsomeness <Handsomenes@yandex.ru>
2023-01-19 21:49:03 +0400

    Решены две задачи.

--- SecondHomework/Program.cs
+++ SecondHomework/Program.cs
@@ -10,16 +10,30 @@ namespace SecondHomework
     {
         static void Main(string[] args)
         {
-            ThreeDigitNumber();
+            //ThreeDigitNumber();
+            //Console.WriteLine();
+            ThirdNumber();
         }
 
         public static void ThreeDigitNumber()
         {
             Console.WriteLine("Введите трёхзначное число");
-            double number = double.Parse(Console.ReadLine());
+            int number = int.Parse(Console.ReadLine());
             string count = number.ToString();
 
-            string result = count.Length < 3 || count.Length > 3 ? "Вы ввели некорректное число" : count.Substring(1, 1);
+            string result = count.Length < 3 || count.Length > 3 ?
+                "Вы ввели некорректное или отрицательное число" : count.Substring(1, 1);
+            Console.WriteLine(result);
+        }
+
+        public static void ThirdNumber()
+        {
+            Console.WriteLine("Введите число");
+            int number = int.Parse(Console.ReadLine());
+            string count = number.ToString();
+
+            string result = count.Length < 3 || count.Substring(0, 1) == "-" ?
+                "В вашем числе нет третьей цифры или оно отрицательное" : count.Substring(2, 1);
             Console.WriteLine(result);
         }
     }
