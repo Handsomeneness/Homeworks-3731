b2cfd83593e00da644ad3e995ec5f3c40e052b6f
Handsomeness <Handsomenes@yandex.ru>
2023-01-19 21:25:04 +0400

    Создан новый проект. Решена одна задача.

---
+++ SecondHomework/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreeDigitNumber();
+        }
+
+        public static void ThreeDigitNumber()
+        {
+            Console.WriteLine("Введите трёхзначное число");
+            double number = double.Parse(Console.ReadLine());
+            string count = number.ToString();
+
+            string result = count.Length < 3 || count.Length > 3 ? "Вы ввели некорректное число" : count.Substring(1, 1);
+            Console.WriteLine(result);
+        }
+    }
+}
