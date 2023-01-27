f107d57686c97149dddcb6e64ba5165d80dcb331
Handsomeness <Handsomenes@yandex.ru>
2023-01-27 13:01:50 +0400

    Решены все задачи 4 ДЗ

---
+++ FourthHomework/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourthHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Message("Введите первое число");
+            double b = Message("Введите второе число");
+            Choice(a, b);
+            Console.WriteLine();
+
+            double value = Message("введите число");
+            ElementsSumm(value);
+            Console.WriteLine();
+
+            int arrLenght = Message("Введите длину массива");
+            int[] array = GenerateArray(arrLenght, 0, 50);
+            PrintArray("Сгенерированный массив", array);
+        }
+
+        
+        public static int Message(string msg)
+        {
+            Console.WriteLine(msg);
+            return int.Parse(Console.ReadLine() ?? "0");
+        }
+
+        #region 25
+        public static void Choice(double a, double b)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Доступные команды");
+            Console.WriteLine("1: -");
+            Console.WriteLine("2: +");
+            Console.WriteLine("3: /");
+            Console.WriteLine("4: *");
+            Console.WriteLine("5: Возведение в степень");
+            Console.WriteLine("Введите команду");
+            int value = int.Parse(Console.ReadLine());
+            switch (value)
+            {
+                case 1:
+                    Console.WriteLine($"Ответ: {a - b}");
+                    break;
+                case 2:
+                    Console.WriteLine($"Ответ: {a + b}");
+                    break;
+                case 3:
+                    Console.WriteLine($"Ответ: {a / b}");
+                    break;
+                case 4:
+                    Console.WriteLine($"Ответ: {a * b}");
+                    break;
+                case 5:
+                    Console.WriteLine($"Ответ: {Math.Pow(a, b)}");
+                    break;
+                default:
+                    Console.WriteLine("Такой команды не существует");
+                    break;
+            }
+        }
+        #endregion
+
+        #region 27
+        public static void ElementsSumm(double value)
+        {
+            string summ = value.ToString();
+            int temp = 0;
+            for (int i = 0; i < summ.Length; i++)
+            {
+                temp += int.Parse(summ.Substring(i, 1));
+            }
+            Console.WriteLine(temp);
+        }
+        #endregion
+
+        #region 29
+        public static int[] GenerateArray(int number, int begin, int end)
+        {
+            Random random = new Random();
+            int[] array = new int[number];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(begin, end);
+            }
+            return array;
+        }
+
+        public static void PrintArray(string msg, int[] array)
+        {
+            Console.Write("[");
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                Console.Write(array[i]+", ");
+            }
+            Console.Write(array[array.Length-1]+"]");
+            Console.WriteLine();
+        }
+        #endregion
+    }
+}
---
+++ TEST/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Программа
+            //   ввод данных
+            int num = ReadData("Введите число: ");
+
+
+            bool result = PalinTest(num);
+            if (result)
+            {
+                PrintData("Число является палиндромом");
+            }
+            else
+            {
+                PrintData("Число не является палиндромом");
+            }
+
+            // вывод данных 
+            
+
+
+        }
+
+        // 1. Метод читает данные от пользователя
+       public static int ReadData(string msg)
+        {
+            Console.WriteLine(msg);
+            return int.Parse(Console.ReadLine() ?? "0");
+        }
+
+
+        // 2. Метод PalinTest - определения палиндрома 
+       public static bool PalinTest(int n)
+        {
+            int d1 = n / 10000;
+            int d2 = (n / 1000) % 10;
+            int d3 = (n / 10) % 10;
+            int d4 = n % 10;
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+
+            bool res = ((d1 == d4) && (d2 == d3)) ? true : false;
+            return res;
+        }
+
+
+        // 3. Метод вывода результата пользователю
+        public static void PrintData(string msg1)
+        {
+            Console.WriteLine(msg1);
+        }
+
+
+    }
+}
